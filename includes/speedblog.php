5bc912611f7bbb622bc34d0e7edd8815c2a63830
whoobagent <md5hushh@gmail.com>
2018-01-18 16:14:45 +0100

    speedblog1.1

--- includes/speedblog.php
+++ includes/speedblog.php
@@ -5,83 +5,88 @@ include 'dbh.inc.php';
 $offset = 0;
 $page_result = 6;
 
+
 if(isset($_GET['cat']))
 
 {
+  /////////////////////////////////////////
+  ///Fetching results based on category///
+  ///////////////////////////////////////
 
-  if($_GET['dog'])
+  $r = $_GET["cat"];
+  $query = "SELECT * FROM blog_articles WHERE blog_category = '$r' LIMIT $offset, $page_result";
+  $result = mysqli_query($conn,$query);
+  $rows = mysqli_num_rows($result);
+  $pagecount = $rows;
+  $num = $pagecount / $page_result;
+
+  if($_GET['catnr]'])
   {
-   $page_value = $_GET['dog'];
+   $page_value = $_GET['catnr'];
    if($page_value > 1)
    {
     $offset = ($page_value - 1) * $page_result;
    }
   }
 
-
-$select_results_all =  "SELECT * FROM blog_articles WHERE blog_category = '$r'";
-$result = mysqli_query($conn,$select_results_all);
-$rows = mysqli_num_rows($result);
-$pagecount = $rows;
-$num = $pagecount / $page_result;
-
-$r = $_GET["cat"];
-$query = "SELECT * FROM blog_articles WHERE blog_category = '$r' ORDER BY blogid DESC limit $offset, $page_result";
-$result = mysqli_query($conn,$query);
-//  var_dump($result);
-//  exit();
-$blogid[]=array();
-$title[]=array();
-$date[]=array();
-$body[]=array();
-$image[]=array();
-$author[]=array();
-$category[] = array();
+  $blogid[]=array();
+  $title[]=array();
+  $date[]=array();
+  $body[]=array();
+  $image[]=array();
+  $author[]=array();
+  $category[]=array();
 
  while($row = mysqli_fetch_array($result))
  {
- $blogid[] = $row[0];
- $title[] = $row[2];
- $body[] = $row[3];
- $author[]= $row[4];
- $date[] = $row[6];
- $image[] = $row[7];
- $category[] = $row[8];
+  $blogid[] = $row[0];
+  $title[] = $row[2];
+  $body[] = $row[3];
+  $author[]= $row[4];
+  $date[] = $row[6];
+  $image[] = $row[7];
+  $category[] = $row[8];
  }
 
-  // var_dump($conn);
-  // exit();
 }
 else
 
 {
-  if($_GET['pagenr'])
-{
- $page_value = $_GET['pagenr'];
- if($page_value > 1)
- {
-  $offset = ($page_value - 1) * $page_result;
- }
-}
 
-$select_results_all =  "SELECT * FROM blog_articles";
-$result = mysqli_query($conn,$select_results_all);
-$rows = mysqli_num_rows($result);
-$pagecount = $rows;
-$num = $pagecount / $page_result;
-$select_results =  "SELECT * FROM blog_articles ORDER BY blogid DESC limit $offset, $page_result";
+  ////////////////////////////////////////////
+  /////Fetching all results and counting/////
+  //////////////////////////////////////////
+
+  $select_results_all =  "SELECT * FROM blog_articles";
+  $result = mysqli_query($conn,$select_results_all);
+  $rows = mysqli_num_rows($result);
+  $pagecount = $rows;
+  $num = $pagecount / $page_result;
+
+  if($_GET['pagenr'])
+  {
+   $page_value = $_GET['pagenr'];
+   if($page_value > 1)
+   {
+    $offset = ($page_value - 1) * $page_result;
+   }
+  }
+
+////////////////////////////////////////////////////////////////////////////////////////
+///Set a limit on all fetched results and create array to import data into the cards///
+//////////////////////////////////////////////////////////////////////////////////////
+
+$select_results =  "SELECT * FROM blog_articles ORDER BY blogid DESC LIMIT $offset, $page_result";
 $result = mysqli_query($conn,$select_results);
 
 
-$blogid[]=array();
-$title[]=array();
-$date[]=array();
-$body[]=array();
-$image[]=array();
-$author[]=array();
-$category[] = array();
-
-
+ $blogid[]=array();
+ $title[]=array();
+ $date[]=array();
+ $body[]=array();
+ $image[]=array();
+ $author[]=array();
+ $category[] = array();
 
 
 
@@ -101,11 +106,10 @@ while($row = mysqli_fetch_array($result))
 }
 
 
+////////////////////////////////////////
+///////Shorten text in the cards///////
+//////////////////////////////////////
 
-
-
-
-//Blogkaartjes
 function custom_echo($i, $length)
 {
   if(strlen($i)<=$length)
